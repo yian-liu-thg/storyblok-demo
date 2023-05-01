5ef1a210cb597f85d1e5c992d30efb207c919e47
yianliu <yianliu@outlook.com>
2023-05-01 15:48:34 +0100

    storyblok init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+	modules: [['@storyblok/nuxt', { accessToken: process.env.STORYBLOK_TOKEN}], '@nuxtjs/tailwindcss'],
 })
